a11f1ff7ce425779763c2596fae58b022d7dac1f
empty-crayon <iamchinmayproxy@gmail.com>
2024-12-22 13:19:46 +0530

    Added buffer for runtime error handling

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 )
@@ -20,20 +21,34 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
 }
 
+// potenial bug: template render should be a two step process
+// first a trial render by writing the template into a buffer
+// if this fails, gracefully handle it
+// if it works then write it to response writer
 func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
 	ts, ok := app.templateCache[page]
 
 	if !ok {
 		err := fmt.Errorf("the template %s does not exist", page)
-		app.serverError(w,r,err)
+		app.serverError(w, r, err)
 		return
 	}
 
-	w.WriteHeader(status)
+	// initialise a new buffer
+	buf := new(bytes.Buffer)
 
-	err := ts.ExecuteTemplate(w, "base", data)
-
+	err := ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
+		return
 	}
+	// if there is no error, only then we send status ok
+	w.WriteHeader(status)
+	// INSTEAD OF:
+	// err := ts.ExecuteTemplate(w, "base", data)
+
+	// Write the contents of the buffer to the http.ResponseWriter. Note: this
+	// is another time where we pass our http.ResponseWriter to a function that
+	// takes an io.Writer.
+	buf.WriteTo(w)
 }
