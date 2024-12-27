b5d2847caaa3284aaae3763b9eab460e60c135cf
empty-crayon <iamchinmayproxy@gmail.com>
2024-12-27 20:30:45 +0530

    updated server config with tls config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -78,6 +78,7 @@ func main() {
 		Addr:         *addr,
 		Handler:      app.routes(),
 		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		TLSConfig: tlsConfig,
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
